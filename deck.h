d146fd85ff2cb775cf4af13335c657cbaf11a6b9
Rhys0303 <164986837+Rhys0303@users.noreply.github.com>
2025-06-08 00:50:14 +0800

    deck.h

--- deck.h
+++ deck.h
@@ -1,1 +1,16 @@
+#ifndef DECK_H
+#define DECK_H
+#include"card.h"
+#include<vector>
+using namespace std;
+class Deck
+{
+public:
+	vector<card> cards;
+	Deck();
+	void shuffle();
+	card draw();
+};
+#endif 
+
 
